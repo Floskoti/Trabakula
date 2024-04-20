24d28340d3c530810278be640ef5fa077ca96a63
StevieG2k2 <jernej.petrovcic@gmail.com>
2024-04-20 18:07:11 +0200

    delujoc flag
    dela flag

--- client/js/skripta.js
+++ client/js/skripta.js
@@ -1,16 +1,41 @@
 function toggleFlagNoSpam(clickedIcon) {
-    var icon = document.querySelector('.fa-regular');
-    clickedIcon.classList.toggle("hidden");
-    icon.classList.toggle("hidden");
+    switch(clickedIcon.id){
+        case "fas1": clickedIcon.classList.toggle("hidden");
+                    var icon = document.querySelector('#far1');
+                     icon.classList.toggle("hidden");
+                     break;
+        case "fas2": clickedIcon.classList.toggle("hidden");
+                    var icon = document.querySelector('#far2');
+                     icon.classList.toggle("hidden");
+                     break;
+        case "fas3": clickedIcon.classList.toggle("hidden");
+                    var icon = document.querySelector('#far3');
+                     icon.classList.toggle("hidden");
+                     break;
+        case "fas4": clickedIcon.classList.toggle("hidden");
+                    var icon = document.querySelector('#far4');
+                     icon.classList.toggle("hidden");
+                     break;
+        default: break;
+    }
 }
 
 function toggleFlagSpam(clickedIcon, event) {
-    var icon = document.querySelector('.fa-solid');
     switch(clickedIcon.id){
         case "far1": clickedIcon.classList.toggle("hidden");
+                    var icon = document.querySelector('#fas1');
                      icon.classList.toggle("hidden");
                      break;
         case "far2": clickedIcon.classList.toggle("hidden");
+                    var icon = document.querySelector('#fas2');
+                     icon.classList.toggle("hidden");
+                     break;
+        case "far3": clickedIcon.classList.toggle("hidden");
+                     var icon = document.querySelector('#fas3');
+                      icon.classList.toggle("hidden");
+                      break;
+        case "far4": clickedIcon.classList.toggle("hidden");
+                    var icon = document.querySelector('#fas4');
                      icon.classList.toggle("hidden");
                      break;
         default: break;
