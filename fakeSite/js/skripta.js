27d45929bae63f5f4c3478001ce6d8e5857c757d
lukagrahor <lukagrahor@gmail.com>
2024-04-21 02:52:15 +0200

    SessionStorage dela

--- fakeSite/js/skripta.js
+++
@@ -1,5 +0,0 @@
-var lastName = sessionStorage.getItem("lastname");
-if(lastName != null) {
-    var showMistakes = document.getElementById("showMistakes");
-    showMistakes.innerHTML = lastName;
-}
