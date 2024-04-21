eeae6bc0a823520980e39bfb35dac8d09f83a901
lukagrahor <lukagrahor@gmail.com>
2024-04-21 11:52:18 +0200

    Last minutes eviornment

--- fakeSite/js/modalSkripta.js
+++ fakeSite/js/modalSkripta.js
@@ -9,9 +9,12 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-modal.style.display = "block";
+    modal.style.display = "block";
+    document.getElementById("shoppingItems").scrollIntoView();
+    document.getElementById("modalText").innerHTML = "<p>Naslednji znak so ponudbe, ki so predobre, da bi bile res.<br/>V veliki večini primerov, gre pri takih ponudbah za potegavščino,<br/>zato moramo biti previdni in preveriti na spletu ali je podjetje zaupanja vredno"
+    document.getElementById("url").style.display = "none";
+    btn.style.display = "none";
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
 modal.style.display = "none";
--- fakeSite/js/shopSkripta.js
+++ fakeSite/js/shopSkripta.js
@@ -1,11 +1,5 @@
 var mistakeFlag = sessionStorage.getItem("mistakeFlag");
 console.log(mistakeFlag);
 if(mistakeFlag != null) {
-    console.log("ehmed " + mistakeFlag);
     document.getElementById("myModal").style.display = "block";
-}
-
-function nextExample() {
-    console.log("aha");
-    document.getElementById("shoppingItems").scrollIntoView();
 }
